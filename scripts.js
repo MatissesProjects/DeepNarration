436606dbbaea5837f011372e8b5393b6fc9970c5
Sora Havok <sora.havok@gmail.com>
2023-07-17 16:32:10 -0700

    Added dummy endpoint fun.

--- scripts.js
+++ scripts.js
@@ -12,21 +12,14 @@ function submitForm(event) {
     event.preventDefault(); // Prevent the default form submission
 
     var scenesTextboxes = document.querySelectorAll("#scenes-container input[type='text']");
+
     var scenesData = scenesTextboxes.forEach(textbox => {
         var formData = {
             prompt: textbox.value//.concat(imagesData)
         };
-        fetch("http://127.0.0.1:5001/getImages", {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(response => {
-            // Handle the response as needed
-            console.log(response.json())
-        })
+        getImages(formData, true)
+        .then(response => response.json())
+        .then(response => console.log(response))
         .catch(error => {
             // Handle any errors
         });
@@ -39,4 +32,22 @@ function submitForm(event) {
 
 
     // Perform the AJAX request to send the data
+}
+
+function getImages(formData, useDummy) {
+    if(useDummy) {
+        // dummyResponse
+    return new Promise((resolve, reject) => {
+        // do something
+        resolve(["output98723423.png", "output9834345423.png", "output343223.png"]);
+      });
+    } else {
+        return fetch("http://127.0.0.1:5001/getImages", {
+                method: "POST",
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+    }
 }
