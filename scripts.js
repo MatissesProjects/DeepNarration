b57a7b17051f73e08f6f210fdcbefa87d61bddfc
Sora Havok <sora.havok@gmail.com>
2023-07-17 16:38:23 -0700

    Fixing dummy scripts.

--- scripts.js
+++ scripts.js
@@ -18,11 +18,8 @@ function submitForm(event) {
             prompt: textbox.value//.concat(imagesData)
         };
         getImages(formData, true)
-        .then(response => response.json())
         .then(response => console.log(response))
-        .catch(error => {
-            // Handle any errors
-        });
+        .catch(error => console.error(error));
     });
 
     // var imagesTextboxes = document.querySelectorAll("#images-container input[type='text']");
@@ -36,11 +33,9 @@ function submitForm(event) {
 
 function getImages(formData, useDummy) {
     if(useDummy) {
-        // dummyResponse
-    return new Promise((resolve, reject) => {
-        // do something
-        resolve(["output98723423.png", "output9834345423.png", "output343223.png"]);
-      });
+        return new Promise((resolve, reject) => {
+            resolve(["output98723423.png", "output9834345423.png", "output343223.png"]);
+        });
     } else {
         return fetch("http://127.0.0.1:5001/getImages", {
                 method: "POST",
@@ -49,5 +44,6 @@ function getImages(formData, useDummy) {
                     "Content-Type": "application/json"
                 }
             })
+            .then(response => response.json())
     }
 }
