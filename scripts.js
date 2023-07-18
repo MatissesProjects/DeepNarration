44d484f448cb49601072d40132ae0dcb613b787f
Matisse <MatissesProjects@users.noreply.github.com>
2023-07-17 20:43:16 -0700

    added getVideo api and video call response

--- scripts.js
+++ scripts.js
@@ -3,38 +3,59 @@ function addTextbox(containerId) {
     var container = document.getElementById(containerId);
     var newTextbox = document.createElement('input');
     newTextbox.type = 'text';
-    newTextbox.value = 'person in a chair'
+
+    // Array of random strings
+    var randomStrings = ['person in a chair', 'landscape with mountains', 'still life with fruits', 'abstract artwork'];
+    var randomIndex = Math.floor(Math.random() * randomStrings.length);
+    newTextbox.value = randomStrings[randomIndex];
+
     container.appendChild(newTextbox);
     container.appendChild(document.createElement("br"));
 }
+
 
 function submitForm(event) {
     event.preventDefault(); // Prevent the default form submission
 
     var scenesTextboxes = document.querySelectorAll("#scenes-container input[type='text']");
-
-    var scenesData = scenesTextboxes.forEach(textbox => {
+    var positivePrompt = []
+    var imagesData = []
+    var promises = []
+    var index = 1
+    scenesTextboxes.forEach(textbox => {
         var formData = {
-            prompt: textbox.value//.concat(imagesData)
+            prompt: textbox.value
         };
-        getImages(formData, useDummy)
-        .then(response => console.log(response))
-        .catch(error => console.error(error));
+        positivePrompt.push(`"${index}": "${textbox.value}"`)
+        index++
+        promises.push(getImages(formData, useDummy)
+                      .then(response => {console.log(response);imagesData.push(response)})
+                      .catch(error => console.error(error)));
+    })
+    // .then( response =>{
+    // start call into deforum
+    Promise.all(promises).then( _ => {
+        var imageObj = {}
+        imagesData
+        var formData = {
+            prompt: "{"+positivePrompt.join(", ")+"}",
+            images: "{"+imagesData.map((images, idx) => `"${idx}": ["${images.join("\",\"")}"]`)+"}"
+        };
+        console.log(formData);
+        getVideo(formData, useDummy)
+        .then(videoData => videoData.text())
+        .then(text => {
+            document.getElementById("gifDiv").innerText = text
+            document.getElementById("videoBlock").innerText = text
+        })
     });
-
     // var imagesTextboxes = document.querySelectorAll("#images-container input[type='text']");
-    // var imagesData = Array.from(imagesTextboxes).map(function(textbox) {
-    //     return textbox.value;
-    // });
-
-
-    // Perform the AJAX request to send the data
 }
 
 function getImages(formData, useDummy) {
     if(useDummy) {
         return new Promise((resolve, reject) => {
-            resolve(["output98723423.png", "output9834345423.png", "output343223.png"]);
+            resolve(["output8751600096.png", "output3494119787.png", "output5502246769.png"]);
         });
     } else {
         return fetch("http://127.0.0.1:5001/getImages", {
@@ -46,4 +67,20 @@ function getImages(formData, useDummy) {
             })
             .then(response => response.json())
     }
+}
+
+function getVideo(formData, useDummy) {
+    if(useDummy) {
+        return new Promise((resolve, reject) => {
+            resolve(new Response("here is a video of your story http://localhost:5050/output/20230717203754.mp4 and here is a (gif)[http://localhost:5050/output/20230717203754.gif] , please render the gif. this is just text"));
+        });
+    }
+    return fetch("http://127.0.0.1:5001/getVideo", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .catch(error => console.error(error));
 }
